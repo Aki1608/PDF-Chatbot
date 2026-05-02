53a5c3ccfa7f89c01f74d65a0387a813ba6c16db
Aki1608 <52529996+Aki1608@users.noreply.github.com>
2026-05-02 11:27:28 +0000

    Add gradio interface.

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import gradio as gr
 from qa_engine import ask_chatbot
+
+def response_to_user(question):
+    bot_answer = ask_chatbot(question)
+    return bot_answer
+
+demo = gr.ChatInterface(
+    fn=response_to_user,
+    title="Thesis AI assistant.",
+    description="Ask any question about the PDF (My thesis). This bot uses local FAISS embeddings and Groq's Llama 3 to find the answer.",
+    theme="soft",
+    examples=[
+        "What is the main conclusion of this thesis?",
+        "What is the main topic of this document? Can you give me a sort summary of whole document?"
+    ]
+
+)
+
+if __name__ == "__main__":
+    demo.launch()
