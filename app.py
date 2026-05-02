8982d6b2dcd87fccbfd822552adb020ed2e5c076
Aki1608 <52529996+Aki1608@users.noreply.github.com>
2026-05-02 13:09:59 +0000

    remove theme from argument.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 from qa_engine import ask_chatbot
 
-def response_to_user(question):
+def response_to_user(question, history):
     bot_answer = ask_chatbot(question)
     return bot_answer
 
@@ -9,7 +9,6 @@ demo = gr.ChatInterface(
     fn=response_to_user,
     title="Thesis AI assistant.",
     description="Ask any question about the PDF (My thesis). This bot uses local FAISS embeddings and Groq's Llama 3 to find the answer.",
-    theme="soft",
     examples=[
         "What is the main conclusion of this thesis?",
         "What is the main topic of this document? Can you give me a sort summary of whole document?"
